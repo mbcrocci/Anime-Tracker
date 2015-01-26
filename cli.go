a50d5301082eb6a493343ed15065608950191103
Maurizio Crocci <mbcrocci@gmail.com>
2015-01-26 23:36:58 +0000

    Basic functionality
    Basic cli usage: can now add, increment, show or remove

--- cli.go
+++ cli.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/codegangsta/cli"
+import (
+	"fmt"
 
-func main() {
+	"github.com/codegangsta/cli"
+)
+
+func RunCli() {
 	app := cli.NewApp()
 	app.Name = "Anime Tracker"
 	app.Usage = "Keep track of the current viewed anime episode"
@@ -12,7 +16,42 @@ func main() {
 			ShortName: "a",
 			Usage:     "Add a new anime to the database",
 			Action: func(c *cli.Context) {
+				if err := addAnime(c.Args().First(), c.Args()[1]); err != nil {
+					fmt.Printf("Can't add new anime because: %v\n", err)
+				}
+			},
+		},
+		{
+			Name:      "remove",
+			ShortName: "r",
+			Usage:     "Remove an anime",
+			Action: func(c *cli.Context) {
+				if err := Remove(c.Args().First()); err != nil {
+					fmt.Printf("Can't Remove because: %v", err)
+				}
+			},
+		},
+		{
+			Name:      "show",
+			ShortName: "s",
+			Usage:     "Show all anime in the database",
+			Action: func(c *cli.Context) {
+				for _, anime := range animeList {
+					fmt.Printf("Title: %s\nEpisode: %s\n\n", anime.Title, anime.Episode)
+				}
+			},
+		},
+		{
+			Name:      "increment",
+			ShortName: "i",
+			Usage:     "Increment the last viewed episode",
+			Action: func(c *cli.Context) {
+				if err := Increment(c.Args().First()); err != nil {
+					fmt.Printf("Cant increment because: %v", err)
+				}
 			},
 		},
 	}
+
+	app.RunAndExitOnError()
 }
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+var db *mgo.Collection
+var animeList []Anime
+
+type Anime struct {
+	Id      bson.ObjectId `bson:"_id"`
+	Title   string        `bson:"title"`
+	Episode int           `bson:"episode"`
+}
+
+func (a *Anime) Increment() {
+	a.Episode += 1
+}
+
+func addAnime(title, episode string) error {
+	ep, err := strconv.Atoi(episode)
+	if err != nil {
+		fmt.Println("Can't conver ", episode, "to string")
+		return err
+	}
+	err = db.Insert(Anime{
+		Id:      bson.NewObjectId(),
+		Title:   title,
+		Episode: ep,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Increment(title string) error {
+	for _, anime := range animeList {
+		if anime.Title == title {
+			anime.Increment()
+
+			err := db.Update(
+				bson.M{"title": anime.Title},
+				bson.M{"$set": bson.M{"episode": anime.Episode}})
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return errors.New("Can't find anime: " + title)
+}
+
+func Remove(title string) error {
+	for _, anime := range animeList {
+		if anime.Title == title {
+			if err := db.RemoveId(anime.Id); err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return errors.New("Can't find anime: " + title)
+}
+
+func main() {
+	session, err := mgo.Dial(os.Getenv("MONGO_URL"))
+	if err != nil {
+		fmt.Printf("Can't connect to mongo\n")
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetSafe(&mgo.Safe{})
+
+	// actualy a collection
+	db = session.DB("test").C("anime")
+
+	// Populate the animeList
+	if err := db.Find(nil).All(&animeList); err != nil {
+		fmt.Printf("Can't find any animes")
+	}
+
+	// Run the cli app (cli.go)
+	RunCli()
+}
